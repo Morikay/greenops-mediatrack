43a86231e4fc4df3450106eb7612e033b8d6a7e9
Morikay <216730185+Morikay@users.noreply.github.com>
2026-04-07 15:11:11 +0200

    Clean the repo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,10 @@
 provider "aws" {
   region  = var.region
   profile = var.aws_profile
+  shared_config_files = ["/home/llesage/.aws/config"]
+  shared_credentials_files = ["/home/llesage/.aws/credentials"]
+  
+
 
   # Tags communs appliques automatiquement a toutes les ressources AWS.
   default_tags {
